23bf84fe189e43555c5909293d0b77a2adce9fb5
Ali Mahmoud <961alimahmoud@gmail.com>
2020-10-29 21:31:00 -0400

    updated questions

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const questions = [
     {
         type: 'input',
         name: 'repository',
-        message: 'Enter the name of your repository on GitHub.',
+        message: 'Enter the name of the repository you wish to use.',
         // We need to validate that user entered at least one word
         validate: function (answer) {
             if (answer.length < 1) {
@@ -44,9 +44,25 @@ const questions = [
         }
     },
     {
+        type: "input",
+        name: "description",
+        message: "Briefly describe your project.",
+      },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Walk through the steps the user should follow (if needed) to install your project',
+        // Validation not required if question is optional
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Mention people or sources that contributed to this project"
+    },
+    {
         type: 'list',
         name: 'license',
-        message: 'Choose your license for your project.',
+        message: 'Choose a license for your project.',
         choices: ['afl-3.0', 'apache-2.0', 'artistic-2.0', 'bsl-1.0', 'bsd-2-clause', 'bsd-3-clause', 'bsd-3-clause-clear', 'cc', 'cc0-1.0', 'cc-by-4.0', 'cc-by-sa-4.0', 'wtfpl', 'ecl-2.0', 'epl-1.0', 'epl-2.0', 'eupl-1.1', 'agpl-3.0', 'gpl', 'gpl-2.0', 'gpl-3.0', 'lgpl', 'lgpl-2.1', 'lgpl-3.0', 'isc', 'lppl-1.3c', 'ms-pl', 'mit', 'mpl-2.0', 'osl-3.0', 'postgresql', 'ofl-1.1', 'ncsa', 'unlicense', 'zlib']
         
     },
