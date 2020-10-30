1f127df5fa213b5adef68ba2c472f5cad1f80bf3
Ali Mahmoud <961alimahmoud@gmail.com>
2020-10-29 21:24:29 -0400

    created questions

--- index.js
+++ index.js
@@ -44,17 +44,12 @@ const questions = [
         }
     },
     {
-        type: "checkbox",
-        message: "What license are you using?",
-        name: "license",
-        choices: ["MIT", "GPLv3", "The Unlicense"],
-        validate: function (answer) {
-          if (answer.length < 1) {
-            return console.log("You must enter the license of your project.");
-          }
-          return true;
-        },
-      },
+        type: 'list',
+        name: 'license',
+        message: 'Choose your license for your project.',
+        choices: ['afl-3.0', 'apache-2.0', 'artistic-2.0', 'bsl-1.0', 'bsd-2-clause', 'bsd-3-clause', 'bsd-3-clause-clear', 'cc', 'cc0-1.0', 'cc-by-4.0', 'cc-by-sa-4.0', 'wtfpl', 'ecl-2.0', 'epl-1.0', 'epl-2.0', 'eupl-1.1', 'agpl-3.0', 'gpl', 'gpl-2.0', 'gpl-3.0', 'lgpl', 'lgpl-2.1', 'lgpl-3.0', 'isc', 'lppl-1.3c', 'ms-pl', 'mit', 'mpl-2.0', 'osl-3.0', 'postgresql', 'ofl-1.1', 'ncsa', 'unlicense', 'zlib']
+        
+    },
 ];
 
 // function to write README file
